8cc7d6f46b99b0694887f528fd12446f6c715340
ldi72 <dmy_litvin@mail.ru>
2021-12-20 16:02:24 +0300

    update

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,30 @@
 <template>
   <ul>
-    <input type="radio" id="one" value="1" v-model="picked" @change="RadioOnChange($event)" checked="checked">
+    <input type="radio" id="one" value="1" v-model="picked">
     <label for="one">Все</label>
     <br>
-    <input type="radio" id="two" value="2" v-model="picked" @change="RadioOnChange($event)">
+    <input type="radio" id="two" value="2" v-model="picked">
     <label for="two">Меньше 0</label>
     <br>
-    <input type="radio" id="three" value="3" v-model="picked" @change="RadioOnChange($event)">
+    <input type="radio" id="three" value="3" v-model="picked">
     <label for="three">Больше или равно 0</label>
     <br>
     <!--span>Выбрано: {{ picked }}</span-->
     <br>
 
-    <input type="checkbox" id="checkbox" @change="check($event)">
+    <input type="checkbox" id="checkbox" v-model="checked">
     <label for="checkbox">Сортировка по дате</label>
   </ul>
 
   <ul style="border-left: solid">
     <li v-for="item in displayData" v-bind:key="item.id">
       <input type="checkbox" v-bind:value="item.id" v-model="selectedAmounts">
-      <label>{{item}}</label><br>
+      <label>{{ item }}</label><br>
     </li>
   </ul>
 
   <ul style="border-left: solid">
-    <li v-for="item in selectedAmounts"   v-bind:key="item">{{ item }}</li>
+    <li v-for="item in selectedAmounts" v-bind:key="item">{{ item }}</li>
     <!--span>Выбрано: {{selectedAmounts}}</span-->
   </ul>
 
@@ -33,26 +33,33 @@
 <script>
 export default {
   name: 'App',
-  data() {
-    let rndData = [], displayData = [], selectedAmounts = []
-    for (let i = 0; i < 100; i++){rndData.push({id: i+1, amount: Math.floor(Math.random() * (1000 - (-1000) + 1)) + (-1000)})}
-    displayData = rndData;
-    return{
-      selectedAmounts,
-      displayData,
-      rndData,
-      picked: ''
+  created() {
+    for ( let i = 0; i < 100; i++) {
+      this.rndData.push({id: i+1, amount: Math.floor(Math.random() * (1000 - (-1000) + 1)) + (-1000)})
     }
   },
-  methods: {
-    check: function(e) {
-      if (e.target.checked) {this.displayData = this.displayData.sort(function (a, b) {return a.amount-b.amount;})}
-      else {this.displayData = this.displayData.sort(function (a, b) {return a.id-b.id;})}
-    },
-    RadioOnChange: function(e) {
-      if (e.target.value == "1") {this.displayData = this.rndData.filter(function(number) {return number.amount;})}
-      if (e.target.value == "2") {this.displayData = this.rndData.filter(function(number) {return number.amount < 0;})}
-      if (e.target.value == "3") {this.displayData = this.rndData.filter(function(number) {return number.amount >= 0;})}
+  data() {
+    return {
+      selectedAmounts: [],
+      rndData: [],
+      picked: '1',
+      checked: false
+    }
+  },
+  computed: {
+    displayData() {
+      let displayData = []
+      if (this.picked == "1") {displayData = this.rndData}
+      else if (this.picked == "2") {displayData = this.rndData.filter(item => item.amount < 0)}
+      else if (this.picked == "3") {displayData = this.rndData.filter(item => item.amount >= 0)}
+
+      if (this.checked) {displayData = displayData.sort((a, b) => a.amount-b.amount)}
+      else {displayData =  displayData.sort((a, b) => a.id-b.id)}
+
+      //if (this.checked) {displayData = displayData.sort(function (a, b) {return a.amount-b.amount;})}
+      //else {displayData =  displayData.sort(function (a, b) {return a.id-b.id;})}
+
+      return displayData
     }
   }
 }
